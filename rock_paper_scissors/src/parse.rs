d38d1298e81c61b5796c251adec3658926cc1c49
George Dragomir <george.dragomir.x@gmail.com>
2022-12-06 22:07:36 +0200

    Optimise parser for day 2

--- rock_paper_scissors/src/parse.rs
+++ rock_paper_scissors/src/parse.rs
@@ -1,26 +1,24 @@
-use chumsky::{
-    primitive::{choice, end, just},
-    text::{newline, TextParser},
-    Parser,
-};
-
-fn round() -> core::BParser<crate::Line> {
-    choice((just('A'), just('B'), just('C')))
-        .then_ignore(just(' '))
-        .then(choice((just('X'), just('Y'), just('Z'))))
-        .map(|(first, second)| crate::Line { first, second })
-        .boxed()
-}
-
-fn parser() -> core::BParser<Box<[crate::Line]>> {
-    round()
-        .separated_by(newline())
-        .map(Vec::into_boxed_slice)
-        .padded()
-        .then_ignore(end())
-        .boxed()
-}
-
 pub fn parse(input: &str) -> Option<Box<[crate::Line]>> {
-    parser().parse(input).ok()
+    input
+        .lines()
+        .map(|line| {
+            if line.len() != 3 {
+                None
+            } else {
+                let mut chars = line.chars();
+
+                match (
+                    chars.next().unwrap(),
+                    chars.next().unwrap(),
+                    chars.next().unwrap(),
+                ) {
+                    (first @ ('A' | 'B' | 'C'), ' ', second @ ('X' | 'Y' | 'Z')) => {
+                        Some(crate::Line { first, second })
+                    }
+                    _ => None,
+                }
+            }
+        })
+        .collect::<Option<Vec<_>>>()
+        .map(Vec::into_boxed_slice)
 }
