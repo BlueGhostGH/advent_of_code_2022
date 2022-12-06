888b6cd109087e92dd9a114c623d3dc8c358d653
George Dragomir <george.dragomir.x@gmail.com>
2022-12-06 21:52:33 +0200

    Optimise parser for day 1

--- calorie_counting/src/parse.rs
+++ calorie_counting/src/parse.rs
@@ -1,29 +1,16 @@
-use chumsky::{
-    primitive::end,
-    text::{int, newline, TextParser},
-    Parser,
-};
-
-fn calories() -> core::BParser<crate::Calories> {
-    int(10).from_str().unwrapped().boxed()
-}
-
-fn elf() -> core::BParser<crate::Elf> {
-    calories()
-        .separated_by(newline())
-        .map(Vec::into_boxed_slice)
-        .boxed()
-}
-
-fn parser() -> core::BParser<Box<[crate::Elf]>> {
-    elf()
-        .separated_by(newline())
-        .map(Vec::into_boxed_slice)
-        .padded()
-        .then_ignore(end())
-        .boxed()
-}
-
 pub fn parse(input: &str) -> Option<Box<[crate::Elf]>> {
-    parser().parse(input).ok()
+    input
+        .lines()
+        .collect::<Vec<_>>()
+        .split(|line| line.is_empty())
+        .map(|elf| {
+            elf.iter()
+                .copied()
+                .map(str::parse::<crate::Calories>)
+                .map(Result::ok)
+                .collect::<Option<Vec<_>>>()
+                .map(Vec::into_boxed_slice)
+        })
+        .collect::<Option<Vec<_>>>()
+        .map(Vec::into_boxed_slice)
 }
